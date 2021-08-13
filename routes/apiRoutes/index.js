175da7a112837834ddc816ca754736d3d0faf747
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-08-13 09:55:34 -0500

    Update POST route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require(`express`).Router();
 const { notes } = require(`../../db/db`); // import JSON file that contains database
+const { createNewNote, validateNote } = require(`../../lib/notes`);
 
 router.get(`/notes`, (req, res) => {
   res.json(notes); // read the db.json file and return all saved notes as JSON
@@ -8,19 +9,12 @@ router.get(`/notes`, (req, res) => {
 router.post(`/notes`, (req, res) => {
   req.body.id = notes.length.toString(); // set id based on what the next index of the array will be
   
-  // validate data
-     // if invalid, send 400 error back
-     // else write notes data to JSON
-
-  console.log(req.body);
-  res.json(req.body); // req.body is where incoming content will be
-});
-
-router.delete(`/notes/:id`, (req, res) => {
-  // Receive a query parameter containing the id of a note to delete
-  // Read all notes from the db.json file
-  // Remove the note with the given id property
-  // Rewrite the notes to the db.json file
+  if (!validateNote(req.body)) { // if invalid, send 400 error back
+    res.status(400).send(`The note is not properly formatted.`);
+  } else { // else write notes data to JSON
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
 });
 
 module.exports = router;
